0f4683b201ed78601743fd2e95a3ca718dbf2a7b
Julián Calderón Almendros <earendil.elrond.arwen@gmail.com>
2025-03-18 21:46:59 +0100

    Segundo intento de subir el proyecto a GitHub

--- first.hs
+++ first.hs
@@ -6,19 +6,13 @@ main :: IO ()
 main = do
     let arg         = 20
     let arg_str     = show arg
-    let result      = fact arg
-    let result_str  = show result
-    let finish      = "factorial (" ++ arg_str ++ ") = " ++ result_str
-    putStrLn finish 
-    let arg         = 17
-    let arg_str     = show arg
-    let result      = euler arg
-    let result_str  = show result
-    let finish      = "exponential_e (" ++ arg_str ++ ") = " ++ result_str
-    putStrLn finish
-    let arg         = 17 
-    let arg_str     = show arg
-    let result      = euleraprox arg
-    let result_str  = show result
-    let finish      = "exponential_e (" ++ arg_str ++ ") = " ++ result_str
-    putStrLn finish
+    let result      = Fact.fact arg
+    putStrLn $ "factorial (" ++ show arg ++ ") = " ++ show result
+
+    let arg1 = 17
+    let result1 = Fact.euler arg1
+    putStrLn $ "exponential_e (" ++ show arg1 ++ ") = " ++ show result1
+
+    let arg2 = 17
+    let result2 = Fact.euleraprox arg2
+    putStrLn $ "exponential_e (" ++ show arg2 ++ ") = " ++ show result2
