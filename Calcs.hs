0d8f8cab63f80f7cc0c0419dbae50f8d322959e7
Julián Calderón Almendros <earendil.elrond.arwen@gmail.com>
2025-03-29 20:57:44 +0100

    Nueva función binom::Integer -> Integer -> Integer

--- Calcs.hs
+++ Calcs.hs
@@ -1,6 +1,6 @@
 module Calcs (fact, euler, euleraprox) where
 
-import Data.Ratio ( (%) )
+import Data.Ratio
 
 fact :: Integer -> Integer
 fact 0 = 1
@@ -16,3 +16,11 @@ euler n = (1 % fact n) + euler (n - 1)
 
 euleraprox :: Integer -> Double
 euleraprox n = fromRational (euler n)
+
+binom :: Integer -> Integer -> Integer
+binom 0   _   = 0
+binom n   0   = 1
+binom n   n   = 1
+binom n   1   = n
+binom n n-1   = n
+binom n   k   = ((n − k + 1) * (binom n  (k − 1))) / k
---
+++ calculos.md
@@ -0,0 +1,6 @@
+
+
+binomial n   m   = binom( n , m )
+(n 1) + (n 2) + (n 3) + ... + (n n) = 2^n
+( n k ) = ( n − 1 k − 1 ) + ( n − 1 k )
+ C ( n , k ) = ( n − k + 1 ) k ⋅ C ( n , k − 1 ) 
