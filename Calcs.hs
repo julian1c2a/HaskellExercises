ee3861f430345b8bf8bc27cf7c7a53935399f1ab
U-TIRITH\julia <julian.calderon.almendros@gmail.com>
2025-03-31 22:18:57 +0200

    Añadiendo tests de pruebas de binom

--- Calcs.hs
+++ Calcs.hs
@@ -19,21 +19,61 @@ euleraprox n = fromRational (euler n)
 
 binom :: Integer -> Integer -> Integer
 binom n k
-  | n < 0       = 0
-  | k < 0       = 0
-  | n < k       = 0
-  | n == 0      = 0
-  | k == 0      = 1
-  | n == k      = 1
-  | k == 1      = n
-  | k + 1 == n  = n
-  | otherwise   = div (n * binom (n - 1) (k - 1)) k
+  | n < 0       = -1
+  | k < 0       = -1
+  | n < k       =  0
+  | n == 0      =  0
+  | k == 0      =  1
+  | n == k      =  1
+  | k == 1      =  n
+  | k + 1 == n  =  n
+  | otherwise   =  div (n * binom (n - 1) (k - 1)) k
 
-testbinom :: Integer -> Bool 
-testbinom n
-  | (n <= 0)&&(binom 0 n == 0) = True 
+test_binom_property_0 :: Integer -> Integer -> Bool
+test_binom_property_0 n k
+  | (n < 0)  && (k < 0)  && (binom n k == -1) = True
+  | (n < 0)  && (k >= 0) && (binom n k == -1) = True
+  | (n >= 0) && (k < 0)  && (binom n k == -1) = True
+  | (n == 0) && binom n k == 0                = True
+  | (n < k)  && binom n k == 0                = True
+  | binom n k == div (fact n) (fact k * fact (n - k)) = True
+  | otherwise = False
+
+test_binom_property_1 :: Integer -> Integer -> Bool
+test_binom_property_1 n k
+  | (n  < 0)  && (k  < 0)  && (binom n k == -1) = True
+  | (n  < 0)  && (k >= 0)  && (binom n k == -1) = True
+  | (n >= 0)  && (k  < 0)  && (binom n k == -1) = True
+  | (n == 0)  && binom n k == 0                 = True
+  | (n < k)   && binom n k == 0                 = True
+  | binom n k == binom n (n - k)                = True
+  | otherwise = False
+
+test_binom_property_2 :: Integer -> Bool 
+test_binom_property_2 n
+  | (n < 0)&&(binom n 1 == (-1)) = True 
   | sum [binom n i | i <- [0..n]] == 2^n = True
   | otherwise = False
   where
     sum [] = 0
     sum (x:xs) = x + sum xs
+
+test_binom_property_3 :: Integer -> Integer -> Bool 
+test_binom_property_3 n k
+  | (n  < 0)  && (k  < 0)  && (binom n k == -1) = True
+  | (n  < 0)  && (k >= 0)  && (binom n k == -1) = True
+  | (n >= 0)  && (k  < 0)  && (binom n k == -1) = True
+  | (n == 0)  && binom n k == 0                 = True
+  | (n < k)   && binom n k == 0                 = True
+  | (k == 0)  && (binom n k == 1)               = True
+  | (n == 1)  && (binom n k == 1)               = True
+  | equaltosumofbinoms n k                      = True
+  | otherwise                                   = False
+    where
+      equaltosumofbinoms n k = binom n k == binom (n - 1) (k - 1) + binom (n - 1) k
+
+
+
+
+
+
---
+++ test/Spec.hs
@@ -0,0 +1,7 @@
+import Test.QuickCheck
+
+prop_reverse :: [Int] -> Bool
+prop_reverse xs = reverse (reverse xs) == xs
+
+main :: IO ()
+main = quickCheck prop_reverse
