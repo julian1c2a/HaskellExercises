bb02a60e4ca8cd339e0d585ff0d1fc1240e6941c
Julián Calderón Almendros <earendil.elrond.arwen@gmail.com>
2025-04-10 22:04:40 +0200

    Terminando la función de polinomios de Bernoulli

--- Calcs.hs
+++ Calcs.hs
@@ -40,13 +40,45 @@ binom n k
   -- DE UN SOLO ELEMENTOS DEL TOTAL: TIENE CARDINAL n
   | otherwise = div (n * binom (n - 1) (k - 1)) k -- FÓRMULA DE RECURRENCIA
 
-bernoulli :: Integer -> Rational
-bernoulli n =
+bernoulli_number :: Integer -> Rational
+bernoulli_number n =
   | n       <  0 =  0 %  1 -- NO HAY NÚMEROS DE BERNOULLI DE ÍNDICE NEGATIVOS
   | n       == 0 =  1 %  1 -- CASO BASE IMPRESCINDIBLE
   | n       == 1 = -1 %  2 -- CASO BASE PRESCINDIBLE
   | n       == 2 =  1 %  6 -- CASO BASE PRESCINDIBLE
-  | n       == 3 = -1 % 30 -- CASO BASE PRESCINDIBLE
+  | n       == 3 =  0 %  1 -- CASO BASE PRESCINDIBLE
+  | n       == 4 = -1 % 30 -- CASO BASE PRESCINDIBLE
   | n mod 2 == 1 =  0 %  1 -- CASO DE DE NÚMEROS DE BERNOULLI IMPARES
-  | otherwise    = sum [binom n k * bernoulli k | k <- [0..(n-1)]] % (n + 1) 
+  | otherwise    = sum [binom n k * bernoulli_number k | k <- [0..(n-1)]] % (n + 1) 
                   -- CASO DE RECURRENCIA GENERAL
+
+-- | Bernoulli numbers are a sequence of rational numbers 
+-- | with many applications in number theory and combinatorics.
+-- | The first few Bernoulli numbers are:
+-- | B_0 = 1
+-- | B_1 = -1/2
+-- | B_2 = 1/6
+-- | B_3 = 0
+-- | B_4 = -1/30
+-- | B_5 = 0
+-- | B_6 = 1/42
+-- | B_7 = 0
+-- | B_8 = -1/30
+-- | B_9 = 0
+-- | B_10 = 5/66
+-- | B_11 = 0
+-- | B_12 = -691/2730
+
+bernoulli_poly :: Integer -> (Rational -> Rational)
+bernoulli_poly n =
+  | n       <= 0 =  \x ->   1 % 1 --CASO BASE IMPRESICINDIBLE
+  | n       == 1 =  \x -> - 1 % 2 + x --CASO BASE PRESCINDIBLE
+  | n       == 2 =  \x ->   1 % 6 - x + x^^2 --CASO BASE PRESCINDIBLE
+  | n       == 3 =  \x ->   (1 % 2) * x − (3 % 2) * x^^2 + x^^3 
+  -- CASO BASE PRESCINDIBLE
+  | n       == 4 =  \x -> − (1 % 30) + x^^2 − 2 * x^^3 + x^^4
+  -- CASO BASE PRESCINDIBLE
+  | otherwise    =  \x -> sum [ (term n k) x | k <- [0..n]]
+  -- CASO DE RECURRENCIA GENERAL
+    where
+      term n k = \x -> binom n k * bernoulli_number k * x^^(n - k)
